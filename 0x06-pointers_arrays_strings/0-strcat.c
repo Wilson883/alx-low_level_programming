df1a07f13464acd55e29f1221b73ac28b5195629
root <Wilsonbenson30@gmail.com>
2022-09-21 15:41:00 -0700

    kff

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,10 @@
 *Return: returns poiner to @dest
 */
 
-
 char *_strcat(char *dest, char *src)
 {
-
-
 	
- int index = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 	
 	 while (dest[index++])
 		 dest_len++;
