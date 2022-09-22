1386a981242ddf5743960f5e461a64e8751e1661
root <Wilsonbenson30@gmail.com>
2022-09-22 04:24:03 -0700

    jfjf

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,17 @@
 *Return: returns poiner to @dest
 */
 
+
 char *_strcat(char *dest, char *src)
 {
-	
-	int index = 0, dest_len = 0;
-	
-	 while (dest[index++])
-		 dest_len++;
-	
-	 for (index = 0; src[index]; index++)
-		 dest[dest_len++] = src[index];
-	
-	 return (dest);
+
+int index = 0, dest_len = 0;
+
+	while ([index++]
+	dest_len++;
+
+for (index= 0; src[index]; index ++)
+		dest[dest_len++] = src[index];
+
+return (dest);
 }
